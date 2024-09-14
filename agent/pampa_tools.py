ad28b5866ffe97919074d10da8fe4c08bb188bcc
lgesuellip <lgesuellipinto@uade.edu.ar>
2024-09-14 11:19:44 -0300

    Add id

--- agent/pampa_tools.py
+++ agent/pampa_tools.py
@@ -1,10 +1,50 @@
-from langchain.tools import BaseTool
+from langchain_core.tools import BaseTool
 from pydantic import BaseModel, Field
 from typing import List, Dict
 import json
 import requests
 from langchain_core.messages import FunctionMessage
 from .storage import StorageStrategy, TinyDBStorage
+from langchain_core.messages import ToolCall
+from typing import Union, Dict, Optional, Any, Tuple
+from langchain.schema.runnable import RunnableConfig
+
+
+class PampaBaseTool(BaseTool):
+    person_id: int = None
+
+    def _prep_run_args(
+        self,
+        input: Union[str, dict, ToolCall],
+        config: Optional[RunnableConfig],
+        **kwargs: Any,
+    ) -> Tuple[Union[str, Dict], Dict]:
+        tool_call_id = input.get("id")
+        tool_input = input.get("args").copy()
+        return (
+            tool_input,
+            dict(
+                callbacks=config.get("callbacks"),
+                tags=config.get("tags"),
+                metadata=config.get("metadata"),
+                run_name=config.get("run_name"),
+                run_id=config.pop("run_id", None),
+                config=config,
+                tool_call_id=tool_call_id,
+                **kwargs,
+            ),
+        )
+
+    def invoke(
+        self,
+        input: Union[str, Dict, ToolCall],
+        config: Optional[RunnableConfig] = None,
+        **kwargs: Any,
+    ) -> Any:
+        tool_input, kwargs = self._prep_run_args(input, config, **kwargs)
+        self.person_id = config.get("metadata", {}).get("thread_id")
+        return self.run(tool_input, **kwargs)
+
 
 class TeamMembersTool(BaseTool):
     name = "team"
@@ -30,18 +70,17 @@ class Expense(BaseModel):
     date: str
     total_value: float
 
-class ExpenseTrackerTool(BaseTool):
+class ExpenseTrackerTool(PampaBaseTool):
     name = "expense_tracker"
     description = "Tracks expenses for team members."
     args_schema = Expense
     storage: StorageStrategy = TinyDBStorage()
 
-
     def _run(self, person: str, expense_type: str, date: str, total_value: float):
         """Adds a new expense to the tracker."""
 
         expense = {
-            "id": person, #TODO id
+            "id": self.person_id,
             "expense_type": expense_type,
             "date": date,
             "total_value": total_value,
@@ -54,19 +93,15 @@ class ExpenseTrackerTool(BaseTool):
         except Exception as e:
             return FunctionMessage(name=self.__class__.__name__, content=f"Error adding expense: {str(e)}")
 
-class ExpenseQuery(BaseModel):
-    query: str
-
-class GETExpenseTrackerTool(BaseTool):
+class GETExpenseTrackerTool(PampaBaseTool):
     name = "get_expenses"
     description = "Retrieves expenses based on a query."
-    args_schema = ExpenseQuery
     storage: StorageStrategy = TinyDBStorage()
 
     def _run(self, query, config):
         """Retrieves expenses based on the provided query."""
         try:
-            expenses = self.storage.get_expenses(query['person'])
+            expenses = self.storage.get_expenses(self.person_id)
             expenses_json = json.dumps(expenses, ensure_ascii=False)
             return FunctionMessage(name=self.__class__.__name__, content=expenses_json)
         except Exception as e:
@@ -84,3 +119,4 @@ class CancelPendingExpensesTool(BaseTool):
             return FunctionMessage(name=self.__class__.__name__, content="All pending expenses have been cancelled successfully.")
         except Exception as e:
             return FunctionMessage(name=self.__class__.__name__, content=f"Error cancelling pending expenses: {str(e)}")
+    
